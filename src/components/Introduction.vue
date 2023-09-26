16cc6b89e5872bf23ee33d667ea5d6babfc1a98b
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-09-26 23:44:41 +0700

    remove unused v-col and center the intro text, make the text smaller

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,12 +1,10 @@
 <template>
   <v-container>
-    <v-col>
-      <h1 class="text-h4 text-lg-h2 text-left pb-5">Welcome to my Portofolio website!</h1>
-      <h1 class="text-h6 text-lg-h4 text-left">
-        My name is Agustinus Nicander Hery, and you can call me Nic. I am a computer science student
-        at <a href="https://h-da.de/">Hochschule Darmstadt</a> and currently in my 6th semester.
-      </h1>
-    </v-col>
+    <h1 class="text-h4 text-lg-h3 text-center pb-5">Welcome to my Portofolio website!</h1>
+    <h1 class="text-h6 text-lg-h4 text-left">
+      My name is Agustinus Nicander Hery, and you can call me Nic. I am a computer science student
+      at <a href="https://h-da.de/">Hochschule Darmstadt</a> and currently in my 6th semester.
+    </h1>
   </v-container>
 </template>
 <script lang="ts" setup>
