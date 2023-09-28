d97fbf6d204441ac88412f3af8d0eec89a497acd
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-09-28 13:56:04 +0700

    lint and format

--- src/components/TechnicalSkills.vue
+++ src/components/TechnicalSkills.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container>
     <h1 class="text-h4 text-lg-h3 text-center pb-5">What are skills that I am familiar with?</h1>
-    <v-row class='elevation-3 py-4 bg-surface' justify='center' no-gutters>
-      <v-col v-for='skill in skills' :key='skill.name' cols='3' lg='2'>
-        <v-img :src='skill.iconUrl' height='4rem' />
-        <p class='text-caption'>{{ skill.name }}</p>
+    <v-row class="elevation-3 py-4 bg-surface" justify="center" no-gutters>
+      <v-col v-for="skill in skills" :key="skill.name" cols="3" lg="2">
+        <v-img :src="skill.iconUrl" height="4rem" />
+        <p class="text-caption">{{ skill.name }}</p>
       </v-col>
     </v-row>
   </v-container>
