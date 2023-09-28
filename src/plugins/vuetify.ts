4fb6153a023f1020c68bf653d3bc44dd8f32108c
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-09-28 13:51:58 +0700

    Change theme to material design 3

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -7,18 +7,27 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
+import { md3 } from 'vuetify/blueprints';
 
 // Composables
 import { createVuetify } from 'vuetify';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  blueprint: md3,
   theme: {
     themes: {
       light: {
         colors: {
           primary: '#1867C0',
           secondary: '#5CBBF6',
+          surface: '#FFFFFF',
+        },
+      },
+      dark: {
+        colors: {
+          primary: '#4CAF50',
+          secondary: '#43A047',
         },
       },
     },
