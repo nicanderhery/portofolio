e157d81d63fd9baf288c43d7cf5c394edd628e4a
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-09-28 11:29:02 +0700

    Add custom directive appear, the component will be hidden, while still taking space

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -7,10 +7,11 @@
 // Plugins
 import vuetify from './vuetify';
 import router from '../router';
+import appear from './appear';
 
 // Types
 import type { App } from 'vue';
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router);
+  app.use(vuetify).use(router).directive('appear', appear);
 }
