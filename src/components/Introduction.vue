b21eb26d20f6911eb72b3fb2fa7c55b1d6b6bcee
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-10-14 19:53:25 +0200

    Adjust div and justify content the text

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,34 +1,36 @@
 <template>
   <v-container>
-    <h1 class="text-h4 text-lg-h3 text-center pb-5">Welcome to my Portofolio website!</h1>
-    <h1 class="text-body-1 text-lg-h4 text-left pb-5">
-      I'm Agustinus Nicander Hery, also known as Nic, a dedicated computer science student at
-      <a href="https://h-da.de/">Hochschule Darmstadt</a>, now in my 6th semester. My journey in
-      programming has led me to explore a diverse set of technologies. I've honed my skills in
-      building robust backends using Express and .NET, while also venturing into front-end
-      development with React.
-    </h1>
-    <h1 class="text-body-1 text-lg-h4 text-left">
-      This semester, I'm excited to delve into advanced web development using React and Chakra UI.
-      Beyond the classroom, I've crafted a practical inventory app for my family using React Native.
-      My ultimate career aspiration is to become a proficient web developer, capable of creating
-      innovative and user-friendly web applications. I thrive on challenges and the opportunity to
-      continuously expand my knowledge in the dynamic world of computer science.
-    </h1>
-  </v-container>
+    <div>
+      <h1 class="text-h4 text-lg-h3 text-center pb-5">Welcome to my Portofolio website!</h1>
+      <h1 class="text-body-1 text-lg-h4 text-justify pb-5">
+        I'm Agustinus Nicander Hery, also known as Nic, a dedicated computer science student at
+        <a href="https://h-da.de/">Hochschule Darmstadt</a>, now in my 6th semester. My journey in
+        programming has led me to explore a diverse set of technologies. I've honed my skills in
+        building robust backends using Express and .NET, while also venturing into front-end
+        development with React.
+      </h1>
+      <h1 class="text-body-1 text-lg-h4 text-justify">
+        This semester, I'm excited to delve into advanced web development using React and Chakra UI.
+        Beyond the classroom, I've crafted a practical inventory app for my family using React
+        Native. My ultimate career aspiration is to become a proficient web developer, capable of
+        creating innovative and user-friendly web applications. I thrive on challenges and the
+        opportunity to continuously expand my knowledge in the dynamic world of computer science.
+      </h1>
+    </div>
 
-  <v-container>
-    <v-btn @click="showDialog = true">Download my CV</v-btn>
-    <v-dialog v-model="showDialog" width="auto">
-      <v-card>
-        <v-card-title class="headline">Choose your language</v-card-title>
-        <v-card-actions>
-          <v-btn color="primary" @click="downloadCV">English</v-btn>
-          <v-spacer />
-          <v-btn color="primary" @click="downloadCV('de')">German</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <v-container>
+      <v-btn @click="showDialog = true">Download my CV</v-btn>
+      <v-dialog v-model="showDialog" width="auto">
+        <v-card>
+          <v-card-title class="headline">Choose your language</v-card-title>
+          <v-card-actions>
+            <v-btn color="primary" @click="downloadCV">English</v-btn>
+            <v-spacer />
+            <v-btn color="primary" @click="downloadCV('de')">German</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-container>
   </v-container>
 </template>
 
