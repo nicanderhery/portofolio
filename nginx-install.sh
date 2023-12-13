98004087797235b9f8a60d8acd8728938c9d260c
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-12-13 23:56:18 +0000

    Update nginx-install.sh script with new config options

--- nginx-install.sh
+++ nginx-install.sh
@@ -1,14 +1,56 @@
 # If the script can not be launched, run the following command first:
 # chmod +x nginx-install.sh
 
-# Because the nginx config is set for location /, the location / of the main nginx config should be commented out.
+# Example config for nginx.conf for frontend
+# config="location /$DIR_NAME {
+#     alias $file_dir;
+#     index index.html;
+#     try_files \$uri \$uri/ =404;
+# }"
+
+# Example config for nginx.conf for backend
+# config="location /api {
+#     proxy_set_header    Host             $host;
+#     proxy_set_header    X-Real-IP        $remote_addr;
+#     proxy_set_header    X-Forwarded-For  $proxy_add_x_forwarded_for;
+#     proxy_set_header    X-Forwarded-Proto https;
+#     proxy_pass http://localhost:9999/api;
+# }
+
 
 function remove_config_and_dist() {
-    sudo rm /etc/nginx/shared/$DIR_NAME.conf
-    echo "nginx config for $DIR_NAME has been removed."
-    sudo rm -r /var/www/html/$DIR_NAME
-    echo "Distribution for $DIR_NAME has been removed."
+    echo "Removing the nginx config and the distribution."
+    sudo rm -rf $file_config
+    sudo rm -rf $file_dir
+    echo "The nginx config and the distribution have been removed."
 }
+
+# Ask the user which domain he would like to use
+echo "What is your domain name? If you do not have one, then just press enter."
+read domain
+
+github_gist=https://gist.github.com/nicanderhery/2c06fb6f5688a59ce29046dd5ed3a512
+html_address=/var/www/html
+DIR_NAME=${PWD##*/}
+file_dir="$html_address/$domain/$DIR_NAME"
+file_config=/etc/nginx/domain_routes/$domain.$DIR_NAME.conf
+config="location / {
+    root $file_dir;
+    index index.html;
+    try_files \$uri \$uri/ =404;
+}"
+
+# Check whether domain is empty, then set file_dir to $html_address/$DIR_NAME
+if [ -z "$domain" ]; then
+    file_dir="$html_address/$DIR_NAME"
+    file_config=/etc/nginx/domain_routes/$DIR_NAME.conf
+fi
+
+# Check if the /var/www/html/$domain directory exists
+if [ ! -d "$html_address/$domain" ]; then
+    echo "The /var/www/html/$domain directory does not exist. Please run the script from $github_gist."
+    exit 1
+fi
 
 # Check whether dist is present or empty
 if [ ! -d "dist" ] || [ -z "$(ls -A dist)" ]; then
@@ -16,9 +58,6 @@ if [ ! -d "dist" ] || [ -z "$(ls -A dist)" ]; then
     echo "npm run build"
     exit 1
 fi
-
-# Get the current directory name
-DIR_NAME=${PWD##*/}
 
 # Check whether the directory's name contains a dot or a space
 if [[ "$PWD" =~ \.|\  ]]; then
@@ -32,39 +71,27 @@ if [ ! -x "$(command -v nginx)" ]; then
     exit 1
 fi
 
-# Check whether nginx is running
-if [ "$(systemctl is-active nginx)" != "active" ]; then
-    echo "nginx is not running. Please start nginx first."
-    exit 1
-fi
-
 # Check whether nginx config within this repository is present
 if [ ! -f "nginx/conf/nginx.conf" ]; then
     echo "nginx config not found. Please run this script from the root of the repository or make sure the nginx config is present in the nginx/conf directory."
     exit 1
 fi
 
-# Check whether nginx config is the correct one
-if ! grep -q "include /etc/nginx/shared/\*.conf;" /etc/nginx/conf.d/nginx.conf; then
-    echo "Main nginx config is not correct. Please run the following script from this gist: https://gist.github.com/nicanderhery/2c06fb6f5688a59ce29046dd5ed3a512/raw/0724800e3991f95084e2ca2ce5e43f137e47e2fb/nginx-certbot.sh"
-    exit 1
+# Create the directory $file_dir if it does not exist
+if [ ! -d "$file_dir" ]; then
+    sudo mkdir -p $file_dir
 fi
 
-# Create a directory for nginx config if it does not exist
-if [ ! -d "/etc/nginx/shared" ]; then
-    sudo mkdir /etc/nginx/shared
+# Create the directory /etc/nginx/domain_routes if it does not exist
+if [ ! -d "/etc/nginx/domain_routes" ]; then
+    sudo mkdir -p /etc/nginx/domain_routes
 fi
 
-# Copy nginx config to /etc/nginx/shared
-sudo cp nginx/conf/nginx.conf /etc/nginx/shared/$DIR_NAME.conf
-
-# Create a directory for the distribution
-if [ ! -d "/var/www/html/$DIR_NAME" ]; then
-    sudo mkdir /var/www/html/$DIR_NAME
-fi
+# Copy nginx config to /etc/nginx/domain_routes
+echo "$config" | sudo tee $file_config > /dev/null
 
 # Copy the distribution to the directory
-sudo cp -r dist/* /var/www/html/$DIR_NAME
+sudo cp -r dist/* $file_dir
 
 # Check whether syntax of nginx config is correct
 if [ ! -x "$(command -v nginx -t)" ]; then
@@ -77,6 +104,7 @@ output=$(sudo nginx -t 2>&1)
 if [[ $output == *"test is successful"* ]]; then
     echo "nginx config for $DIR_NAME is valid."
 else
+    echo "$output"
     echo "nginx config for $DIR_NAME is invalid. Please check the nginx config."
     remove_config_and_dist
     exit 1
